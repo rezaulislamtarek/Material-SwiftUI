a79fc6355f133927c3da54bfd8f6560fcdc1761e
rezaulislamtarek <mrezaulislamtarek.t@gmail.com>
2023-08-01 17:04:35 +0600

    Secure field

--- Sources/Material SwiftUI/MSField.swift
+++ Sources/Material SwiftUI/MSField.swift
@@ -6,24 +6,24 @@
 //
 
 import SwiftUI
+ 
 
 @available(iOS 15.0, *)
 public struct MSField: View {
-    @Binding public var text: String
-    public var placeHolder: String
-    var color: Color
-    var cornerRadius: Float
+    @Binding public var text:String
+    public var placeHolder : String
+    var color : Color
+    var cornerRadious : Float
     @State public var isTapped = false
-    @State private var showPassword = false // Step 1: Add showPassword state
-
-    public init(text: Binding<String>, placeHolder: String, isTapped: Bool = false, color: Color, cornerRadius: Float) {
+    
+    public init(text: Binding<String>, placeHolder: String, isTapped: Bool = false, color: Color, cornerRadious : Float) {
         self._text = text
         self.placeHolder = placeHolder
         self.isTapped = isTapped
         self.color = color
-        self.cornerRadius = cornerRadius
+        self.cornerRadious = cornerRadious
     }
-
+    
     public var body: some View {
         ZStack(alignment: .leading) {
             Text(placeHolder)
@@ -34,44 +34,53 @@ public struct MSField: View {
                 .offset(y: isTapped ? -28 : 0)
                 .offset(x: isTapped ? -8 : 0)
                 .scaleEffect(isTapped ? 0.8 : 1, anchor: .leading)
-
-            if showPassword { // Step 2: Use SecureField for password input
-                SecureField("", text: $text)
-                    .font(.system(.title2, design: .rounded))
-            } else {
-                TextField("", text: $text)
-                    .font(.system(.title2, design: .rounded))
-            }
             
-            // Step 3: Add a button to toggle showPassword state
-            HStack{
-                Spacer()
-                Button(action: {
-                    showPassword.toggle()
-                }){
-                    Image(systemName: showPassword ? "eye.slash" : "eye")
-                        .foregroundColor(showPassword ? .accentColor : .black.opacity(0.5))
+//            SecureField("", text: $text){ status in
+//                withAnimation(.easeIn){
+//                    isTapped = true
+//                }
+//            } onCommit: {
+//                    if text == "" { withAnimation(.easeOut){
+//                        isTapped = false
+//                    }
+//                }
+//            }
+//            .onSubmit {
+//                    if text == "" { withAnimation(.easeOut){
+//                        isTapped = false
+//                    }
+//                }
+//            }
+            SecureField("", text: $text)
+                .onSubmit {
+                    if text == "" {
+                        withAnimation(.easeOut){
+                            isTapped = false
+                        }
+                    }
                 }
-            }
-             
+            //.font(.system(.title2, design: .rounded))
+            
+            
             
         }
         // For ZStack
+         
         .padding(.horizontal)
-        .padding(.vertical, 10)
+        .padding(.vertical,10)
         .background(
-            RoundedRectangle(cornerRadius: CGFloat(cornerRadius))
+            RoundedRectangle(cornerRadius: CGFloat(cornerRadious))
                 .stroke(isTapped ? Color.accentColor : Color.gray.opacity(0.5), lineWidth: 1)
         )
     }
 }
-
-@available(iOS 15.0.0, *)
-struct MSField_Previews: PreviewProvider {
-
-    static var previews: some View {
-        MSField(text: .constant(""), placeHolder: "Enter text", color: Color.white, cornerRadius: 10.0)
-            .previewLayout(.sizeThatFits)
-            .padding()
+    
+    @available(iOS 15.0.0, *)
+    struct MSField_Previews: PreviewProvider {
+        
+        static var previews: some View {
+            MSTextField(text: .constant(""), placeHolder: "Enter text", color: Color.white, cornerRadious: 10.0)
+                .previewLayout(.sizeThatFits)
+                .padding()
+        }
     }
-}
